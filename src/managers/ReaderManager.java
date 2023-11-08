346d67673d8f51bc085094f3147cc8b8de6a9f6f
prostiida <131643806+NataliaLuneva@users.noreply.github.com>
2023-11-08 15:04:01 +0200

    Update ReaderManager.java

--- src/managers/ReaderManager.java
+++ src/managers/ReaderManager.java
@@ -6,6 +6,7 @@
 package managers;
 
 import enttity.Reader;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -14,7 +15,6 @@ import java.util.Scanner;
  */
 public class ReaderManager {
 
-    
     private final Scanner scanner;
 
     public ReaderManager(Scanner scanner) {
@@ -22,7 +22,7 @@ public class ReaderManager {
     }
 
     public Reader addReader() {
-        System.out.println("-------Add new reader-------");
+        System.out.println(" ----- Add new reader -----");
         Reader reader = new Reader();
         System.out.print("Firstname: ");
         reader.setFirstname(scanner.nextLine());
@@ -30,23 +30,24 @@ public class ReaderManager {
         reader.setLastname(scanner.nextLine());
         System.out.print("Phone: ");
         reader.setPhone(scanner.nextLine());
-        System.out.print("Added reader " + reader.toString());
+        System.out.println("Added reader " + reader.toString());
         return reader;
     }
 
-    public int printListReaders(Reader[] readers) {
+    public int pirntListReaders(List<Reader> readers) {
         int count = 0;
-        System.out.println("--------List readers-------- ");
-        for(int i = 0; i < readers.length; i++) {
+        System.out.println("List readers: ");
+        for (int i = 0; i < readers.size(); i++) {
             System.out.printf("%d. %s. %s. %s%n",
-            i + 1,
-            readers[i].getFirstname(),
-            readers[i].getLastname(),
-            readers[i].getPhone()
+                    i+1,
+                    readers.get(i).getFirstname(),
+                    readers.get(i).getLastname(),
+                    readers.get(i).getPhone()
             );
-            count ++;
+            count++;
         }
         return count;
     }
+    
     
 }
